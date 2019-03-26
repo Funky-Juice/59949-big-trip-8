c363eff7b9093589c0a1a789857a2848bcd6d9a1
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-03-26 16:43:05 +0300

    создал класс для api

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import {renderFilters, renderTripPoints} from './screens/trips-screen';
 import {pointsData, filtersList} from './data/data';
+import Api from './api';
 import './menu';
 
+const AUTHORIZATION = `Basic dXNlckBwYXNzd29yZAo=${Math.random()}`;
+const API_URL = `https://es8-demo-srv.appspot.com/big-trip`;
+
+const api = new Api(API_URL, AUTHORIZATION);
 
 renderFilters(filtersList, pointsData);
 renderTripPoints(pointsData);
