85e59ba67e33881bc5c7eea165328e3b123de3fe
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-04-01 12:26:28 +0300

    проксирование в провайдере методов api

--- src/services/provider.js
+++ src/services/provider.js
@@ -1,2 +1,31 @@
 
-export default class Provider {}
+export default class Provider {
+  constructor({api, store}) {
+    this._api = api;
+    this._store = store;
+  }
+
+  getTripPoints() {
+    return this._api.getTripPoints();
+  }
+
+  getDestinations() {
+    return this._api.getDestinations();
+  }
+
+  getOffers() {
+    return this._api.getOffers();
+  }
+
+  updateTripPoint({id, data}) {
+    return this._api.updateTripPoint({id, data});
+  }
+
+  createTripPoint({data}) {
+    return this._api.createTripPoint({data});
+  }
+
+  deleteTripPoint({id}) {
+    return this._api.deleteTripPoint({id});
+  }
+}
